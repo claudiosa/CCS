af7b3b299ec3b27f56bc1fa15f7efe370222658a
Claudio Cesar de Sa <claudio.sa@udesc.br>
2016-03-05 16:35:25 -0300

    commiting

---
+++ alp_eletrica_course/alp_codes/01_oi_turma_oi_turma.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main()
+	{
+	printf("\n  Oi turma de ALP\n ");
+	return(1);
+	}
--- alp_eletrica_course/alp_codes/pgm01.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-
-int main()
-	{
-	printf("\n  Oi turma de ALP\n ");
-	return(1);
-	}
