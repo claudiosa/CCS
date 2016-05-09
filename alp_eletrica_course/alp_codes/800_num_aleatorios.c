573885a5b41d04d6ca4bb42a247b774eca34a8e2
Claudio Cesar de Sá <claudio.sa@udesc.br>
2016-05-09 15:54:43 -0300

    atualizacoes do gerador de numeros aleatorios

--- alp_eletrica_course/alp_codes/800_num_aleatorios.c
+++ alp_eletrica_course/alp_codes/800_num_aleatorios.c
@@ -8,10 +8,11 @@ int main() {
  
   printf("\n Quantos números aleatórios inteiros até 1000: ");
   scanf("%d", &Limite);
+  printf("%d \n", Limite);
   for (c = 1; c <= Limite; c++) {
     n = rand() % 1000 + 1;
     printf("%d ", n);
   }
- 
+  printf("\n \n");
   return 0;
 }
