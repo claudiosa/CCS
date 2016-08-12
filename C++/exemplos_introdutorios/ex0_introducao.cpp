811f2bb7bd45a382154b96530880d66396533028
claudiosa <claudio.sa@udesc.br>
2016-08-12 14:12:23 -0300

    exemplos C++

---
+++ C++/exemplos_introdutorios/ex0_introducao.cpp
@@ -0,0 +1,30 @@
+/*
+ * Mateus Boiani - mateuz.boiani@gmail.com 		
+ * 
+ * Agosto / 2016
+ *
+ * Introdução a C++
+ * 
+ * Universidade do Estado de Santa Catarina
+ *
+ * Departamento de Ciência da Computação
+*/
+#include <iostream>
+
+/*
+ * Iostream é uma diretiva do pré-processador. Indica que usaremos
+ * entrada/saída stream C++;
+ *
+*/
+
+int main(){
+	/*std :: Especifica que estamos usando um nome que pertence
+	* ao espaço de nomes std. 
+	* 
+	* << indica stream de saída
+	*/
+	std::cout << "Bem-vindo ao C++!" << std::endl;
+
+	std::cout << "Imprimindo\nmultiplas\nlinhas\nem\nC++\n";
+	return 0;
+}
--- alp_eletrica_course/alp_codes/001_oi_turma.c
+++ alp_eletrica_course/alp_codes/001_oi_turma.c
@@ -3,5 +3,6 @@
 int main()
 	{
 	printf("\n  Oi turma de ALP\n ");
+	printf("\n  Wellcome .... \n ");
 	return(1);
 	}
