45de379c156e179f8ee7b623351ec6eacadaaf15
claudio.sa@udesc.br <claudio.sa@udesc.br>
2016-10-22 11:15:54 -0200

    latex

---
+++ bash_scripts_cc/install_sublime_2.sh
@@ -0,0 +1,9 @@
+# Download Sublime Text 2 from http://www.sublimetext.com/2
+# If you aren't root, sudo su
+### EM /usr/local e com download OK   ... Sublime Text 2.0.2 x64.tar.bz2 ... uso esta versao
+tar -xvjf Sublime\ Text\ 2*.tar.bz2
+mv Sublime\ Text\ 2/ /usr/local/sublime-text-2/
+ln -s /usr/local/sublime-text-2/sublime_text /usr/bin/sublime-text
+ln -s /usr/local/sublime-text-2/sublime_text /usr/local/bin/sublime_text
+###rm Sublime\ Text\ 2*.tar.bz2  apague depois manualmente
+#### modified by CCS
