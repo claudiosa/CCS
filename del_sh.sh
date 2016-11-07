9fd5a5f6518864aaa034314a892b2435e34ca5ea
claudiosa <claudio.sa@udesc.br>
2016-11-07 12:46:55 -0200

    minizinc

--- del_sh.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-rm minizinc/experimental_graph_library/lib/order_fn.mzn~
-rm minizinc/experimental_graph_library/lib/vertex_colouring_fn.mzn~
-rm minizinc/experimental_graph_library/lib/graphs.mzn~
-rm minizinc/experimental_graph_library/lib/vertex_set_fn.mzn~
-rm minizinc/experimental_graph_library/lib/is_adj_matrix_square.mzn~
-rm minizinc/experimental_graph_library/examples/connected_ex.mzn~
-rm minizinc/lista-warmup01-MFO/lab-minizinc-mfo.tex~
-echo " ......... TERMINOU "
