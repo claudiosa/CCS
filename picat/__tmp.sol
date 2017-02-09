b01cbb0451fb8b775f1e04256e29e2f3eb819514
claudiosa <claudio.sa@udesc.br>
2017-02-09 10:11:22 -0200

    slides picat

---
+++ picat/__tmp.sol
@@ -0,0 +1,32 @@
+Problem:    
+Rows:       5
+Columns:    3 (1 integer, 0 binary)
+Non-zeros:  6
+Status:     INTEGER OPTIMAL
+Objective:  obj = 20 (MAXimum)
+
+   No.   Row name        Activity     Lower bound   Upper bound
+------ ------------    ------------- ------------- -------------
+     1 r.4                       -20           -20               
+     2 r.5                        20            -4               
+     3 r.6                  -5.55556           -20               
+     4 r.7                   5.55556             0               
+     5 r.8                       -80           -80             = 
+
+   No. Column name       Activity     Lower bound   Upper bound
+------ ------------    ------------- ------------- -------------
+     1 X0                         20            -4            20 
+     2 X1                    5.55556             0            20 
+     3 X_1          *              0             0             = 
+
+Integer feasibility conditions:
+
+KKT.PE: max.abs.err = 0.00e+00 on row 0
+        max.rel.err = 0.00e+00 on row 0
+        High quality
+
+KKT.PB: max.abs.err = 0.00e+00 on row 0
+        max.rel.err = 0.00e+00 on row 0
+        High quality
+
+End of output
