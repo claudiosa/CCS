514afbbe13461dbd9cb8af5625e987eee6b2600b
claudiosa <claudio.sa@udesc.br>
2017-03-29 11:38:41 -0300

    sma

--- alp_eletrica_course/alp_codes/100_maior_menor.c
+++ alp_eletrica_course/alp_codes/100_maior_menor.c
@@ -1,24 +1,26 @@
 #include <stdio.h>
+   
+    int main(void)
+    { // inicio de bloco
+      int a, b, MAIOR, MENOR;
+   
+    printf("\n ENTRADA de 2 NUMEROS (separados por espacos): ");
+    scanf("%d %d", & a, & b );
+    // ESCRITA DE VALORES
+    printf(" SAIDA: \n");
 
-int main()
-  {
-	int a, b, MAIOR, MENOR;
-	
-	 printf("\n ENTRADA de 2 NUMEROS (separados por espacos): ");
-	 scanf("%d   %d",  & a, & b );
-// ESCRITA DE VALORES	  
-	 printf(" SAIDA: \n");
-	 if(a>=b)
-	 {
-		MAIOR = a;
-		MENOR = b;
-	 }
-	 else	
-	 {
-		MAIOR = b;
-		MENOR = a;
-	 }
-  printf("\n MAIOR: %d :  MENOR: %d",MAIOR,MENOR );
-  printf("\n \n Profs. are humans !!!! \n\n");
- return(1);
-}
+    if (  a >= b ) // condicao LOGICA -- then = entao
+       {          // INICIO DE BLOCO = {
+         MAIOR = a;
+         MENOR = b;
+        }             // FIM DE BLOCO = }
+        else    // CASO CONTRARIO
+        {             // INICIO DE BLOCO = {
+          MAIOR = b;
+          MENOR = a;
+        } // FIM DE BLOCO = }
+      
+    printf("\n MAIOR: %d : MENOR: %d",  MAIOR,   MENOR );
+    printf("\n \n Profs. are humans !!!! \n\n");
+    return(1);
+    }  // FIM DE BLOCO = }
