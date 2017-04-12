66438c109beecce92805e0b5d7079a0f0738e717
claudiosa <claudio.sa@udesc.br>
2017-04-12 00:31:49 -0300

    alp

---
+++ alp_eletrica_course/alp_codes/901_string_cmp.cpp
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <string.h>
+
+int main ()
+{
+   char str1[15];
+   char str2[15];
+   int ret;
+
+
+   strcpy(str1, "abcdef");
+   strcpy(str2, "ABCDEF");
+
+   ret = strcmp(str1, str2);
+
+   if(ret < 0)
+   {
+      printf("str1 is less than str2");
+   }
+   else if(ret > 0) 
+   {
+      printf("str2 is less than str1");
+   }
+   else 
+   {
+      printf("str1 is equal to str2");
+   }
+   
+   return(0);
+}
