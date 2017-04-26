8e3ba0049838bd69c08cdc098682f1ff6ebc6568
claudio.sa@udesc.br <claudio.sa@udesc.br>
2017-04-26 16:24:30 -0300

    26_04

--- alp_eletrica_course/alp_codes/508_array-1D.c
+++ alp_eletrica_course/alp_codes/508_array-1D.c
@@ -2,9 +2,12 @@
 // SOMA DO CONTEUDO DE UM VETOR
 #include <stdio.h>
 int main(){
-     int vetor[10] = {3, 41, 1, 2 ,6, 7, 8, 9, -9, -7};
+     int vetor[] = {3, 41, 1, 2 ,6, 7, 8, 9, -9, -7};
      // criando um vetor com 10 posicoes
-     int i, n, soma=0;
+     int i, n, soma = 0; //SOMA EH INTEIRO
+     int menor , MAIOR;
+     menor = MAIOR = vetor[0];
+     
      n = (float) (sizeof(vetor) / sizeof(int));//coerse ... coerção   
      // forçando que a divisão ocorra como fosse para
      // reais
@@ -14,12 +17,24 @@ int main(){
      for(i=0; i < n ;  i++ ){
      printf("\n PARCIAIS: %d || %d || %d",  vetor[i], soma, (soma +  vetor[i]) );
      soma = soma +  vetor[i];
+      
+     if(MAIOR  < vetor[i]) { // TROCA
+		 MAIOR = vetor[i];}
+  
+     if(menor  >= vetor[i]) { // TROCA
+		 menor = vetor[i];}
  
      }
      printf("\n Soma FINAL: %d", soma);
      float MEDIA = (float) soma/n;
      printf("\n Valor Medio: %0.3f\n", MEDIA );
-     printf("\n Valor Medio: %0.3f\n", (float) soma/n );
+     printf("Valor Medio: %0.3f\n", (float) soma/n );
+     for(i=0; i < n ;  i++ ){
+     printf("     V[%d]: %d", i,  vetor[i]);
+     }
+    
+     printf("\n menor: %d", menor );
+     printf("\n MAIOR: %d", MAIOR );
  
  return 0;
 }
