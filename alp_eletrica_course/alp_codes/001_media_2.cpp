f2c2ad4931a2af9f0596618171ba43f8d6d788c5
claudiosa <claudio.sa@udesc.br>
2017-07-06 16:17:42 -0300

    diversos

--- alp_eletrica_course/alp_codes/001_media_2.cpp
+++ alp_eletrica_course/alp_codes/001_media_2.cpp
@@ -3,15 +3,16 @@
 int main(void)
 {
    float nota1, nota2, media;
-  scanf("%f %f",&nota1, &nota2);	
+   printf("\n DIGITE AS DUAS NOTAS: ");
+   scanf("%f %f",&nota1 , &nota2);
    media = (nota1 + nota2) / 2;
-   printf("\n Media entre %f e %f = %f", 
+   printf("\n Media entre %f e %f = %f",
           nota1, nota2, media);
-   return (1);
+   return (1); // FIM
 }
 /*
-udesc@lab:~/joao$ g++  media.cpp 
-udesc@lab:~/joao$ ./a.out  <  entrada.txt > saida.txt 
+udesc@lab:~/joao$ g++  media.cpp
+udesc@lab:~/joao$ ./a.out  <  entrada.txt > saida.txt
 $ ./a.out  <  entrada.txt
 A media entre 100.555550 e           200.555557 = 150.555557
 
