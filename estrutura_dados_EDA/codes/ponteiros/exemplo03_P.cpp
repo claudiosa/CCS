83d0349ebba20a66df057f45f7473748089ae9c5
claudiosa <claudio.sa@udesc.br>
2017-08-12 18:01:38 -0300

    eda

--- estrutura_dados_EDA/codes/ponteiros/exemplo03_P.cpp
+++ estrutura_dados_EDA/codes/ponteiros/exemplo03_P.cpp
@@ -8,7 +8,7 @@ int main(void)
    //  char *lista = "Ola como vai?"; ... HUM ...
    char *pt;
    pt = vetor; //mais saudavel => pt = &vetor[0];
-
+   // valido para char apenas
    system("clear");
    printf("O tamanho de \"%s\" e %d caracteres.\n", vetor, strtam( pt ));
    printf("\n ... Acabou ....");
--- estrutura_dados_EDA/codes/ponteiros/exemplo04_P.cpp
+++ estrutura_dados_EDA/codes/ponteiros/exemplo04_P.cpp
@@ -6,11 +6,12 @@ int main(void)
 	float matriz [50][50];
     float *pt_float;
     int count;
-    pt_float = matriz[0];
-    for (count=0;count<2500;count++)
+    pt_float = matriz[0]; // OU &matriz[0];
+    // mas nao pt_float = matriz;
+    for (count=0;count < 2500 ; count++)
     {
      *pt_float = 0.0;
-     pt_float ++;
+      pt_float ++;
     }
    printf("\n ... Acabou ....\n");
    return 1;
