8a8e30e61991114c24ca8d6c79e5946d194fc042
claudiosa <claudio.sa@udesc.br>
2017-08-15 11:44:21 -0300

    eda

--- estrutura_dados_EDA/curso_slides/backup_EDA.sh
+++ estrutura_dados_EDA/curso_slides/backup_EDA.sh
@@ -2,12 +2,6 @@
 
 rm -f *.acn *.alg *.aux *.bbl *.blg *.brf *.fls *.glsdefs *.glo *.ilg *.ind *.ist *.lof *.log *.lol *.lot *.maf *.mtc* *.nav *.out *.snm *.spl *.synctex.* *.toc *.upa *.upb *.vrb 
 echo "Feito CLEAN"
-rm -r ~ccs/Dropbox/cursos/eda/curso_slides/
-echo "removido destino"
-
-cd ..
-cp -r curso_slides/  ~ccs/Dropbox/cursos/eda/
+rsync   -avzrul   --delete  ~ccs/Dropbox/CCS/estrutura_dados_EDA/ ~ccs/Dropbox/cursos/eda/estrutura_dados_EDA/
 echo "Feito Backup"
-
-cd curso_slides/
 echo "OK ... passou!"
