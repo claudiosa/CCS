19036aded3b1f5491a47a3d7a04e31d8814e1126
claudiosa <claudio.sa@udesc.br>
2017-09-04 08:46:19 -0300

    EDA

--- estrutura_dados_EDA/codes/pilha/pilha06_realloc.c
+++ estrutura_dados_EDA/codes/pilha/pilha06_realloc.c
@@ -7,7 +7,7 @@ typedef struct
 	{
 	  int topo ;
 	  int tam_MAX;
-	  char * pt_entradas;
+	  char * entradas;
 	}  tipo_pilha ;
 
 /*
@@ -25,22 +25,28 @@ struct
 int pilha_cheia  (tipo_pilha * P);
 int pilha_vazia  (tipo_pilha * P);
 
+// aumentar dinamicamente a pilha sob demanda
+void aumenta_pilha(tipo_pilha * P)
+{
+ P -> tam_MAX = (P -> tam_MAX) * 2; // uma heuristica aqui
+ P -> entradas = (char *) realloc(P -> entradas, (P -> tam_MAX) * sizeof(char) );
+ printf(" *2 "); // apenas para ver  esta chamada
+} 
+
+
 // Adicionar x na pilha
 void push(char x, tipo_pilha * P)
 {
-   if ( pilha_cheia ( P ))
-   /* voltando 1 ou true se estiver estah cheia */
-   { printf("\n A PILHA ESTAH CHEIA ");
-     getchar();
-     return;
-    } /* ou seja ... entrada[1] vai ter o 1o. elemento
-         empilhado... estamos perdendo entrada[0]... */
-    ++ (P -> topo); // cuidar ... se ja se esta incrementando
+   if ( pilha_cheia ( P )) /* voltando 1 ou true se estiver estah cheia */
+        aumenta_pilha( P );
+   
+    // apos o incremento insere ... 
+     ++ (P -> topo); // cuidar ... se ja se esta incrementando
     // entao nao vai usar a posicao 0
-    *( P -> pt_entradas + (P -> topo) ) = x;
+    *( P -> entradas + (P -> topo) ) = x;
     
     printf("\t ||PUSH:: %c, TOPO:: %d", 
-     *( P -> pt_entradas + (P -> topo) ) , P->topo);
+     *( P -> entradas + (P -> topo) ) , P->topo);
     
     return;
   } /* fim da funcao push */
@@ -56,9 +62,9 @@ void pop (char * x, tipo_pilha * P)
 	}
 	 // CUIDAR .... x novamente retorno por referencia PONTEIRO
    // se nao for por referencia ... usar *(....)
-	 x  = ( P -> pt_entradas + (P -> topo) );
+	 x  = ( P ->  entradas + (P -> topo) );
 	  printf("\t ||POP:: %c, TOPO:: %d", 
-	  *( P -> pt_entradas + (P -> topo) ) , P->topo);
+	  *( P -> entradas + (P -> topo) ) , P->topo);
     // ajusta topo
 	 -- (P -> topo);
 	 return;
@@ -69,7 +75,8 @@ int pilha_cheia  (tipo_pilha * P)
 { /* estarah cheia se o topo for maior que o maximo - 1 permitido...
      entao retorna TRUE ou 1
   */
- return( P -> topo >= (P -> tam_MAX) );
+ return( P -> topo > (P -> tam_MAX) );
+ // ou  P -> topo == (P -> (tam_MAX-1)
  /* FALSE ==> 0
     TRUE ===> 1
  */
@@ -84,27 +91,39 @@ int pilha_vazia  (tipo_pilha * P)
  */
 }
 // vir como paramento MAX
-void inicializa_pilha ( tipo_pilha * P)
-{   P -> topo = -1 ; // para comecar a pilha com 0
-	//X -> tam_MAX =  JA INICIALIZADO na leitura
-	// mas fica como opcao aqui ...
-	P -> pt_entradas = (char *) malloc( (P -> tam_MAX) * sizeof(char) );
-	if (P -> pt_entradas == NULL)                     
+tipo_pilha * inicializa_pilha (void)
+{   // aqui criada dinamicamente
+    tipo_pilha * P = (tipo_pilha *) malloc( sizeof(tipo_pilha *) );
+    if (P == NULL) 
+    {
+        printf("\n Erro! PILHA NAO CRIADA ....\n");
+        getchar();
+        exit(0);
+    }
+   
+
+    P -> topo = -1 ; // para comecar a pilha com 0
+    P -> tam_MAX =  1; // poderia ser maior ... alocar 1 inicialmnete
+    
+    P -> entradas = (char *) malloc( (P -> tam_MAX) * sizeof(char) );
+	  if (P -> entradas == NULL)                     
     {
         printf("\n Erro! Memoria NAO alocada.\n");
         exit(0);
     }
- 
+  return (P);
 } // comece em 0 se for o caso
 
 
 void destroi_pilha ( tipo_pilha * P )
  {
 	 if(P)
-	 { if (P -> pt_entradas)
-		 free(P -> pt_entradas);
-	  //free(P); AQUI PERDE A REFERENCIA DELA
-	 printf("\n Pilha Desalocada com SUCESSO\n"); 
+	 { if (P -> entradas)
+		 free(P -> entradas);
+	   
+      free(P); //AQUI PERDE A REFERENCIA DELA
+ 	    printf("\n Pilha Desalocada com SUCESSO\n"); 
+
 	 }
 	 else
 	 printf("\n Pilha INEXISTENTE\n"); 	 
