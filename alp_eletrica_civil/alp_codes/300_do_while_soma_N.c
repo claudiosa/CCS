d12a549ceb8e127d22f7e0bd9d2b17a87db01503
claudio.sa@udesc.br <claudio.sa@udesc.br>
2017-09-25 16:13:00 -0300

    div

--- alp_eletrica_civil/alp_codes/300_do_while_soma_N.c
+++ alp_eletrica_civil/alp_codes/300_do_while_soma_N.c
@@ -1,19 +1,20 @@
 #include <stdio.h>
- 
-int main () {
+int main (void) {
 
    /* local variable definition */
    int S = 0; 
-   int N;
+   int N , i = 0;
    printf(" Digite um inteiro MAIOR que 0 :");
    scanf("%d", &N );
-   
+   i = N;
    /* do loop execution */
    do {
-       printf("\n CONT N: %d \t S:%d", N, S);
-       S = S + N;
-       N = N - 1;
-   } while( N > 0 ); // FIM DO BLOCO ou do DO-WHILE
+       S = S + i;
+       printf("\n  ==>CONT i: %d \t S:%d", i, S);
+       i = i - 1;
+	         
+   //} while( i <= N );
+   } while( i >= 0 ); // FIM DO BLOCO ou do DO-WHILE
  
    printf("\n Valor desta Soma é: %d \n", S);
    return 0;
