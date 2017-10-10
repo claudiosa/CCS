ad16f1c60af321a11df74cc0d18976f1d8ea132b
Claudio Cesar de Sá <claudio.sa@udesc.br>
2017-10-10 18:26:41 -0300

    Update 505-array-1D.cpp

--- alp_eletrica_civil/alp_codes/505-array-1D.cpp
+++ alp_eletrica_civil/alp_codes/505-array-1D.cpp
@@ -1,18 +1,23 @@
 #include <stdio.h>
 int main(){
   
-	 int reg []= {-9999,9999,0,0};	 
-	 // A variavel REG vai armazenar o MAIOR, menor e
-	 // as posicoes destes do vetor
-	 int tamanho;
-	 scanf("%d", &tamanho);
+   int reg []= {-9999,9999,0,0};	 
+  // A variavel REG vai armazenar o MAIOR, menor e
+  // as posicoes destes, respectivamente,  no vetor 
+  // simplesmente poderia ser	   int reg [4];
+   int tamanho;
+   scanf("%d", &tamanho);
 	  
     int vetor[tamanho];
     // LEITURA DO VETOR 
     for(int i=0; i < tamanho ; i++ ){
 	   scanf("%d", &vetor[i]);
     }
-        
+   // INICIALIZANDO O MAIOR E O MENOR ... uma estratégia
+    reg[0] =  vetor[0];
+    reg[1] =  vetor[0];	
+	    
+	
     // LACO DE VERIFICACAO NO VETOR 
     for(int i=0; i < tamanho ; i++){
       if( vetor[i] >= reg[0])
