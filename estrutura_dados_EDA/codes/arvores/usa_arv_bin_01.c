045ac4a35d1563854c41fce49f08409fe612f1e1
claudiosa <claudio.sa@udesc.br>
2017-11-03 19:08:02 -0200

    eda

--- estrutura_dados_EDA/codes/arvores/usa_arv_bin_01.c
+++ estrutura_dados_EDA/codes/arvores/usa_arv_bin_01.c
@@ -1,27 +1,20 @@
  // Definicao de arv_bin011.h
 #include "arv_bin.h"
-
-/*
-   void CreateTree(arv_bin **root);
-   arv_bin * InsertTree(arv_bin * X, char * Y, int Z);
-   void DeleteNodeTree( arv_bin **p );
-   arv_bin * TreeSearch(arv_bin *root, char * target)  ;
-   void Inorder(arv_bin * X );
-   void Preorder(arv_bin * X ) ;
-	void Postorder(arv_bin * root );
-	arv_bin * Del_No_2( arv_bin * raiz, char * X );
-
-*/
+#define K 5
 
 int main(void)
    {
       system("clear");
     
       const char *vetor[]= {
-      "JOINVILLE","PARIS","VENEZA","BRUXELAS",
-      "VANCOUVER","LONDRES","MOSCOU","LUXEMBURGO",
-      "GLASCOW", "OXFORD","YORK", "AUSTIN",
-      "MONTREAL" };
+      "TOKIO","PARIS","VENEZA","BRUGES",
+      "MACAU","LONDRES","MOSCOU","MUNIQUE",
+      "GLASCOW", "OXFORD","YORK", "AULNAT",
+      "MONTREAL" }; // 13 cidades
+      const int chaves[] = {13, 17, 23, 33,
+                            //13, 17, 23, 33, // testes de duplicatas
+                            36, 77, 43, 73,
+                            37, 87, 47, 79, 90}; // 13 chaves
    
       /* criar um ponteiro de arvore .... */
       BinTreeNode *pt_TREE;
@@ -29,37 +22,39 @@ int main(void)
       
       // DADOS DA APLICACAO
       TreeEntry  Xdados ; // se quiser *pt_X = &Xdados;
-
-      for (int seq = 0 ; seq < 5 ; seq++ )
+      // adicionando DADOS a arvore
+      for (int seq = 0 ; seq < K ; seq++ )
       {  
-// use dados via PONTEIRO        
+// uso dados via PONTEIRO        
 //     pt_X -> UM_INT = seq;  
 //     pt_X -> UM_NOME = vetor[seq];  
 // OU diretamente       
-       strcpy(Xdados.UM_NOME , vetor[seq]);
-       Xdados . UMA_CHAVE = rand()%100; // NUM aleatorio mesmo
-       // RELEMBRANDO strcpy(Copia,Original);
-		   pt_TREE = InsertTree_BIN( pt_TREE, Xdados );
-       //printf("\n %d : %x : %s", seq, pt_TREE, Xdados.UM_NOME );
+       strcpy(Xdados.UM_NOME , vetor[seq]);// RELEMBRANDO strcpy(Copia,Original);
+       Xdados.UMA_CHAVE = chaves[seq];
+       // Xdados . UMA_CHAVE = rand()%100; // NUM aleatorio mesmo
+       pt_TREE = InsertTree_BIN( pt_TREE, Xdados );
+       
        printf(":%d : %d : %s (End: %x) \t", seq, Xdados.UMA_CHAVE, Xdados.UM_NOME, pt_TREE );
-      }
+      }// fim da entrada
 
       // UMA BUSCA
       BinTreeNode *consulta;
-      int ALVO;
-    
-      printf("\n Qual dado alvo: ");
-      scanf("%d", &ALVO);
-      //printf("\n ALVO: %d", ALVO);
-      consulta = TreeSearch(pt_TREE, ALVO);
+      //int ALVO;
+      //printf("\n Qual dado alvo: ");
+      //scanf("%d", &ALVO);
+      //printf("\t lido ALVO: %d", ALVO);
+      for (int seq = 0 ; seq < K ; seq++ ) // automatizando
+      {  
+      consulta = TreeSearch(pt_TREE, chaves[seq]);
       if(consulta == NULL)
-      printf("\n NAO HA TAL ALVO OU CHAVE \n");
+      printf("\n NAO HA TAL ALVO -- CHAVE NA ABB \n");
       else
       printf("\n Resposta:  %d : %s : END: %x  ",  
         consulta -> dados.UMA_CHAVE, consulta-> dados.UM_NOME,
         consulta );  
-   
-
+      }
+    
+    printf("\n NUMERO DE NOSH: %d", sizeof_Tree_BIN( pt_TREE) ); 
     printf("\n SAIDA IN: \n");  
     printInorder( pt_TREE );
 
@@ -69,14 +64,18 @@ int main(void)
     
     printf("\n SAIDA POS: \n");  
     printPosorder( pt_TREE );
- */   
-
-
-    if ( deltree( pt_TREE ) == true)
+ 
+if ( deltree( pt_TREE ) == true)
     printf("\n ARVORE LIBERADA OK \n");  
     else 
     printf("\n ARVORE NAO LIBERADA");  
     
+ */   
+
+    //deltree( pt_TREE );
+    deltree_TRUE( &pt_TREE );
+    //pt_TREE  = NULL;  
+    printf("\n NUMERO DE NOSH: %d", sizeof_Tree_BIN( pt_TREE) ); 
 	
 return 1;
    }
