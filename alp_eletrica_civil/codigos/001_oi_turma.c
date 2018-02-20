f5450703d1307ab922a6c629b59f214178f6d1de
claudiosa <claudio.sa@udesc.br>
2018-02-19 23:19:20 -0300

    Remove duplicated directory

--- alp_eletrica_civil/codigos/001_oi_turma.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
- int main() //obrigatoria 
-	{ // bloco de inicio
-	printf( " \n  Oi turma de ALP" );
-	printf("\n  Wellcome .... 2017");
-	printf("\n  ACERTEI .... \n ");
-	return 1 ;
-	} // fecha o bloco 
