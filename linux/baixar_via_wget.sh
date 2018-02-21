1ed1f15e41c830040a9aba2c3e990272b39a9300
claudiosa <claudio.sa@udesc.br>
2018-02-21 11:47:58 -0300

    xxx

---
+++ linux/baixar_via_wget.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+wget https://raw.githubusercontent.com/claudiosa/CCS/master/linux/rapidas_linux.tex
+wget https://raw.githubusercontent.com/claudiosa/CCS/master/linux/rapidas_linux.pdf
+wget --recursive --no-parent https://raw.githubusercontent.com/claudiosa/CCS/master/linux/figuras/
+wget -m -e robots=off --no-parent https://raw.githubusercontent.com/claudiosa/CCS/master/linux/figuras/
+echo TODOS na AREA
+pause
+clear
