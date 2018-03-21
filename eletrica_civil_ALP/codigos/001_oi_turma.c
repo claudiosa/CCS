3852b8e251d6cfdb415d81b38eb958c573b9d560
claudiosa <claudio.sa@udesc.br>
2018-03-21 11:08:22 -0300

    dfa

--- eletrica_civil_ALP/codigos/001_oi_turma.c
+++ eletrica_civil_ALP/codigos/001_oi_turma.c
@@ -4,6 +4,10 @@
 	{ // bloco de inicio
 	printf( " \n  Oi turma de ALP" );
 	printf("\n  Wellcome .... 201X");
+<<<<<<< HEAD
+	printf("\n  Linguagem C => TOP \n ");
+=======
 	printf("\n  Linguagem C => TOP ");
+>>>>>>> a9ceee146dd9bf0b5e7aa6f5d62a2065315b57b3
 	return 1 ;
 	} // fecha o bloco 
