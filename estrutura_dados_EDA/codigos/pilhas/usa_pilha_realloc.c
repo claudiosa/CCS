9043df8190d61ac151e412e7a07765894a8108cc
claudiosa <claudio.sa@udesc.br>
2018-04-06 17:39:00 -0300

    pilha

--- estrutura_dados_EDA/codigos/pilhas/usa_pilha_realloc.c
+++ estrutura_dados_EDA/codigos/pilhas/usa_pilha_realloc.c
@@ -1,30 +1,8 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-// ALOCACAO DINAMICA
-// se DUVIDAS -> https://www.ime.usp.br/~pf/algoritmos/aulas/footnotes/typedef.html
-
-typedef struct
-	{
-	  int topo ;
-      int tam_MAX;  // vira de uma STRING DADA #include <string.h>
-	  char * entradas; // em funcao do tamanho desta
-	}  tipo_pilha ;
-
-// ALGUMAS FUNCOES SEMI-PRONTAS
-
-void carrega_pilha ( char * pt_vetor , tipo_pilha * P );
-tipo_pilha * inicializa_pilha (void);
-void imprime_pilha ( tipo_pilha * Pilha );
-int  pilha_cheia  (tipo_pilha * P);
-void push (char x, tipo_pilha * P);
-void pop (char *x, tipo_pilha * P);
-void destroi_pilha ( tipo_pilha * P );
+#include "pilha_realloc.h"
 
 int main(void)
 {
 	system("clear"); // CUIDADO
-	
     // le uma entrada
     printf(" DIGITE UMA ENTRADA :") ; // OK
     // UMA APLICACAO ..... pode estar em um 3o. arquivo
@@ -34,14 +12,14 @@ int main(void)
      tipo_pilha * pt_P = inicializa_pilha ( );
 	//Cria e aloca pilha ... mas sem dados
 	// Da entrada e coloca na PILHA
-	  carrega_pilha ( & entrada [0] , pt_P ); // via push
+    carrega_pilha ( & entrada [0] , pt_P ); // via push
     imprime_pilha ( pt_P ); // via pop
     destroi_pilha ( pt_P );
     
    return 1;
   }
 
-//// conteudo de pt_vetor .... usando push em X
+//APLICACAO // conteudo de pt_vetor .... usando push em X
 void carrega_pilha ( char * pt_vetor , tipo_pilha * P )
 {		
 	printf("\n LEITURA DA ENTRADA ==> %s \n" , pt_vetor);
