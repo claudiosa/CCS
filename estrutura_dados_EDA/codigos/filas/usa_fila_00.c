3448e7a22ae6bafac469624cceecea4bfa84f824
claudiosa <claudio.sa@udesc.br>
2018-04-21 14:54:08 -0300

    ..

--- estrutura_dados_EDA/codigos/filas/usa_fila_00.c
+++ estrutura_dados_EDA/codigos/filas/usa_fila_00.c
@@ -1,6 +1,3 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <stdbool.h> // bool no C
 #include "fila_00.h"
 
 int  main(void)
@@ -8,33 +5,35 @@ int  main(void)
 {   // APLICACAO
 	system("clear");
 	char vetor[]= "XYZW";
+	// entrada caracteres .....
 	// TAMANHO LIMITADO ....
-	int tamanho = (int)sizeof(vetor) - 1;
+	int tamanho = (int) sizeof(vetor) - 1;
 	int j;
 
 	/* criar por referencia para nao duplicar os dados.... */
 	fila * pt_FILA = cria_fila (  tamanho );
 
-	for ( j = 0 ; vetor[j]!= '\0' ; j ++ ) 
-		{	
-		// FACA UM SCANF AQUI ....	
+	for ( j = 0 ; vetor[j]!= '\0' ; j ++ )
+		{
+		// FACA UM SCANF AQUI ....
 		chegada ( vetor[j] , pt_FILA );
+		// push da vida
 		}
-
 		puts("\n FIM das CHEGADAS \n====================================\n");
 		//TESTES
-		//chegada ( '$' , pt_FILA );	
-		//chegada ( '#' , pt_FILA );	
+		//chegada ( '$' , pt_FILA );
+		//chegada ( '#' , pt_FILA );
 
 		while ( fila_vazia  ( pt_FILA) != true )
 			{
-				printf("\t => %c|%d", partida ( pt_FILA ), tamanho_fila( pt_FILA ) );
-            }
+			printf("\t => %c|%d", partida ( pt_FILA ), tamanho_fila( pt_FILA ) );
+			//pop
+      }
 		puts("\n FIM das PARTIDAS \n====================================");
 		//TESTES
 		//printf("....\t => %c ", partida ( pt_FILA ));
-		//printf("....\t => %c ", partida ( pt_FILA ));	
+		//printf("....\t => %c ", partida ( pt_FILA ));
 
 	destroi_fila( pt_FILA ); 
-   return 1;			
+   return 1;
   } /* fim do main */
