796ddcc7b763ae461b7d0573abd84610a94861d1
claudiosa <claudio.sa@udesc.br>
2018-04-25 22:50:22 -0300

    eda

--- estrutura_dados_EDA/codigos/filas/fila_00.h
+++ estrutura_dados_EDA/codigos/filas/fila_00.h
@@ -1,11 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h> // bool no C
+
 // ONDE FORES USAR ESTAS FUNCOES #include "fila_00.h"
  struct STRUCT_FILA
   {
     int  front ;  		/*  ou primeiro ou cabeca da fila */
     int  rear ;	  		/*  ou ultimo ou cauda da fila */
-    int  capacidade ; // limite da fila		 	
+    int  capacidade ; // limite da fila
 	//char entradas[ MAX_FILA ];
-	char * entrada ;	// VOCE
+	 char * entrada ;	// VOCE e seus INTEIROS
+   // apenas um ponteiro para uma STRING
 };
 // e ainda
 typedef  struct STRUCT_FILA  fila ;
@@ -15,8 +20,8 @@ typedef  struct STRUCT_FILA  fila ;
 	bool  fila_cheia  ( fila * F);
 	bool  fila_vazia  ( fila * F);
 	int   tamanho_fila ( fila * F );
-	void  chegada ( char x, fila * F); // entra ... char
-	char  partida ( fila * F );        // .. sai char
-    void  destroi_fila( fila *F);
-    void  mostra_fila( fila *F );
+	void  chegada ( char x, fila * F); // Push ... char
+	char  partida ( fila * F );        // pop .. sai char
+  void  destroi_fila( fila *F);
+  void  mostra_fila( fila *F );
     /************************************************/
