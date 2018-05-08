95eb41dbccaf6f21bfea96c9d6d1750f4b03702a
claudiosa <claudio.sa@udesc.br>
2018-05-07 22:08:55 -0300

    lfa

--- estrutura_dados_EDA/codigos/filas/BFS_fila/bfs_fila.c
+++ estrutura_dados_EDA/codigos/filas/BFS_fila/bfs_fila.c
@@ -1,38 +1,37 @@
 #include "fila_INT.h"
-
 // original http://www.codingalpha.com/depth-first-search-algorithm-c-program/ 
-// By CCS
+// BFS by CCS -- 2018
 // 
 #define MAX 100
  
 #define aberto 1 // ou NAO VISITADO -- true o false -- na fila
 #define visitado 2  // NOH FECHADO
- 
-void lendo_grafo(void);
-void graph_traversal(void);
-void BFS(int vertex, fila *);
+  
+void lendo_grafo(void); // reading the graph
+void graph_traversal(void); // visiting
+void BFS(int vertex, fila *); // using a queue
 
-
-// GLOBAIS .... 
+// VARIAVEIS GLOBAIS ....  simplificacao ...
 int vertices; // TOTAL DE VERTICES
 int adjacent_matrix[MAX][MAX];
 int vertex_status[MAX]; // VETOR de nos abertos ou fechados
  
 void imp_NUM_LETRAS(int ); 
 int vertex_status[MAX];
- 
+//==================================================================== 
 int main(void)
 {
       lendo_grafo(); // leitura da matriz de adjacencia
       graph_traversal();
       return 0;
 }
- 
+//==================================================================== 
 void graph_traversal(void)
 {
-	  fila * F = cria_fila ( MAX ); // criando FISICAMENTE a FILA
+	  fila * F = cria_fila ( MAX ); 
+	  // criando FISICAMENTE a FILA .... MUITOS ESQUECEM isto e ... segmentation fault
 	  int vertex; 
-	  for(vertex = 0; vertex < vertices; vertex++)
+	  for(vertex = 0; vertex < vertices; vertex++) 
 	  { // marca que nenhum foi visitado 
 		vertex_status[vertex] = aberto;
 	  } 
@@ -45,11 +44,11 @@ void graph_traversal(void)
 	  printf("\n fim do BFS \n");
 
 }
- 
+//==================================================================== 
 void BFS(int vertex, fila * F)
 {
   int count;
-  chegada(vertex, F);  // adiciona de imediato a na RAIZ fila
+  chegada(vertex, F);  // adiciona de imediato a na RAIZ fila ou no de partida
   while( ! fila_vazia (F) ) //eh verdade em caso de fila VERDADE
   {
   	vertex = partida(F);	// toma o cabeca da FILA
@@ -57,15 +56,16 @@ void BFS(int vertex, fila * F)
   	{ // verifica se eh no aberto
   		  //printf("%3d", vertex);
   		  imp_NUM_LETRAS(vertex);
-        // AQUI PODE INSERIR UM TESTE QUALQUER
+        // AQUI PODES INSERIR UM TESTE QUALQUER
         // AQUI EH A VISITA ... so imprime
   		  vertex_status[vertex] = visitado;
   		  // muda o status
   	 }
   // BFS pela esquerda ou pela direita .... veja explicacao em sala  
-        for(count = vertices - 1; count >= 0; count--)
-  //     pela direita
-  //		for(count = 0; count < vertices ; count++)
+  // for(count = vertices - 1; count >= 0; count--) // PILHA
+  //  pela direita ao contrário da PILHA a numeracao segue do menor ao maior
+  // logo:
+  	for(count = 0; count < vertices ; count++)
   	    {
   	     if((adjacent_matrix[vertex][count] == 1) && 
              (vertex_status[count] == aberto))
@@ -79,9 +79,8 @@ void BFS(int vertex, fila * F)
   }// FIM DO WHILE 
  return; 
 }
-
 // ha versoes recursivas do BFS ... mas nao eh o caso 
- 
+//====================================================================
  void imp_NUM_LETRAS(int N)
  {
   int a[26]={'a','b','c','d','e','f','g','h','i','j','k',
@@ -90,13 +89,13 @@ void BFS(int vertex, fila * F)
 
     if(N > 26 && N < 0) // até 26 vertices...
 	 {
-	   printf("\n problema ... imp_NUM_LETRAS");
+	   printf("\n Problema ... imp_NUM_LETRAS");
 	   return;
 	 }
 	printf(" %c", toupper(a[ N ])); // ver tabela ASCII
   return;
  }  
-	 
+//====================================================================	 
 
 // LE E ESCREVE 
 void lendo_grafo(void) // MATRIZ ADJACENCIA
@@ -132,3 +131,4 @@ void lendo_grafo(void) // MATRIZ ADJACENCIA
      
      return; 
 }
+//====================================================================
