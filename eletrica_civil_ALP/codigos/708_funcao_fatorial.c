a64c7745bc011bf0dc32be5f537349caa15bfef1
Claudio Cesar de Sá <claudio.sa@udesc.br>
2018-10-31 17:00:00 -0300

    Update 708_funcao_fatorial.c

--- eletrica_civil_ALP/codigos/708_funcao_fatorial.c
+++ eletrica_civil_ALP/codigos/708_funcao_fatorial.c
@@ -3,37 +3,43 @@
 #include <math.h>
 #include <stdlib.h>
 
-long int  func_fat( int ) ;
+long int  func_fat( int ) ; // DECLARAR A FUNCAO
+// MOSTRAR QUE A FUNCAO EH VISIVEL NESTE ARQUIVO
 
 int main(void) {
 
 long int RESPOSTA;
 int x, n;
 
-printf("\n Quanto n voce quer? " );
+printf("LONG bytes %ld\n", sizeof(long int) );
+printf("INT bytes %ld\n", sizeof(int) );
+printf("\n Quantos N voce quer? " );
 scanf("%d" , &n);
 
-for (x = 1; x <= n; x++)
+for (x = 0; x <= n; x++)
 {
-RESPOSTA = func_fat(x);
+RESPOSTA = func_fat( x );// 1, 2, 3, ... n argumento 1 por vez
 printf("\n %d tem o valor do fatorial igual: %ld", x, RESPOSTA );
 }
-printf("\n \n ACABOU O main \n" );
+printf("\n \n ACABOU O MAIN \n" );
 return(1);
 } // FIM DO MAIN
 
-
 /////////////////////////////////////////////////////////////////
 
-long int func_fat( int n)
+long int func_fat ( int n )
 {
   long int Resp_fat;
   Resp_fat = 1;
-  for ( ; n > 1; n-- )
+  printf("\n" );
+  for ( ; n >= 1; n-- )
   {
-     Resp_fat = Resp_fat * n;
+     Resp_fat =  n * Resp_fat;
+     printf("* ");
   };
-
-
+   printf(" ==> na função!!!!" );
   return(Resp_fat)  ;
 }
+
+
+/////////////////////////////////////////////////////////////////
