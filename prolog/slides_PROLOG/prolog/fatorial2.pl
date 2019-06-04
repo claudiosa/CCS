8c8041e8e37871cde18ee14e2881025b5e447885
claudiosa <claudio.sa@udesc.br>
2019-06-04 18:44:08 -0300

    slides picat

--- prolog/slides_PROLOG/prolog/fatorial2.pl
+++
@@ -1,6 +0,0 @@
-fatorial( 0, 1 ).
-fatorial( X, Fat ) :-
-                         X  >  0,
-                        Aux  is  (X - 1),
-                        fatorial( Aux , Parcial ),
-                        Fat  is  ( X   *  Parcial ).
