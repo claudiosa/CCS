d2b157915c6a7fe29ec2935acde31a8d0f32af56
claudiosa <claudio.sa@udesc.br>
2019-06-21 01:04:23 -0300

    slides picat

--- prolog/search_algorithms/maze_BEST_FS.pl
+++ prolog/search_algorithms/maze_BEST_FS.pl
@@ -7,7 +7,7 @@
 %%% good IDEA for a Maze or graphs problem
 
 %%% THIS IDEA is cool
-w(0,0).
+w(0,0). w(1,0).
 w(0,1). w(1,1). w(2,1). w(3,1). w(4,1). w(5,1).
         w(1,2).         w(3,2).         w(5,2).
         w(1,3).         w(3,3).         w(5,3).
@@ -16,7 +16,7 @@ w(0,4). w(1,4). w(2,4).         w(4,4). w(5,4).
 
 %%% Add by CCS
 start_point(0,0). %%% departure, initial ... 
-end_point(4,5). %% arrival, exit, end ...
+end_point(5,5). %% arrival, exit, end ...
 
 
 %%%% GREAT IDEA 
@@ -27,68 +27,112 @@ next_w(X0,Y0,X,Y0) :- X is X0+1. %%% DOWN
 next_w(X0,Y0,X0,Y) :- Y is Y0-1. %%% LEFT
 next_w(X0,Y0,X,Y0) :- X is X0-1. %%% UP
 %%% PERFORMANCE
-
-%%% KERNEL of SEARCH
-search_BEST_FS(X,Y,Path,Path) :-
-           end_point(X,Y).
-           %
-           % format("\n FOUND A SOLUTION\n").
-
-search_BEST_FS(X0,Y0, PATH_TEMP, Path) :-
-    
-    expand_current_node(X0,Y0,L),
-    heuristic_evalution(L, L_H),
-    choice_the_best(L_H, (X1,Y1)),
-    \+ member(  (X1,Y1) , PATH_TEMP ),
-    search_BEST_FS(X1,Y1,[ (X1,Y1)|PATH_TEMP],Path).
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%% end of KERNEL 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%%% expanding for immediate neighbours 
-expand_current_node(X0,Y0,L):-
-     findall((X1,Y1), d(X0,Y0,X1,Y1), L).
-
-%%% evalution functions -- Manhathan distance         
-heuristic_evalution( [], [] ).   
-heuristic_evalution([(X1,Y1)|L], [(X1,Y1)/H1 | L_H ]) :-
-        
-        end_point(X_d,Y_d),
-        H1 is abs(X_d - X1) +    abs(Y_d - Y1),
-        heuristic_evalution( L, L_H ).  
-             
-
-choice_the_best(L_H, (X_best,Y_best)) :-
-      min_value(L_H, (X_best,Y_best) ).
-
-
-%%% FINDING THE MINIMAL HEURISTIC VALUE      
-min_value([ (X,Y)/_ ], (X,Y) ):- !. %% RETURNING HERE THE MINIMAL VALUE
-
-min_value([(X1,Y1)/H1 , (X2,Y2)/H2 | L_H ], MIN_UNKNOW ) :- 
-      H1 =< H2, 
-      min_value([(X1,Y1)/H1 | L_H ], MIN_UNKNOW ). 
-      
-min_value([(X1,Y1)/H1 , (X2,Y2)/H2 | L_H ], MIN_UNKNOW ) :- 
-      H1 > H2, 
-      min_value([(X2,Y2)/H1 | L_H ], MIN_UNKNOW ).
  
  
 %%% MY contribution and corrections    
 %%% one_solution(Solution) :-
  main :-    
           start_point(X0,Y0),
-          search_BEST_FS(X0,Y0, [(X0,Y0)],Path),
+          %%%             NODE  OPEN       CLOSE, SOL
+          search_BEST_FS( X0, Y0, [(X0,Y0)], [], Path),
+          %%% LIST OF LIST ... many possible paths will be explored
           reverse(Path, Solution),
           print(Solution),
           heuristic_sum(Solution, S),
-          format("\n LOWEST SUM is: ~d", S) 
+          format("\n LOWEST SUM is: ~d", S)
           .
+main:- format("\n NONE SOLUTION\n").
 
 %
 % main :- findall(X, one_solution(X), L),
 %         imp_lst(L).            
-    
 
+%%% KERNEL of SEARCH
+search_BEST_FS(X,Y , Path , _ , Path) :-
+           end_point(X,Y),
+           format("\n FOUND A SOLUTION\n"),
+           !.
+
+%search_BEST_FS(CURRENT , CLOSED,  OPEN, SOL )
+search_BEST_FS( X0,Y0 , [(X0,Y0) | L_CLOSED], L_OPEN, SOL ) :-
+    
+    append( L_OPEN, L_CLOSED, ALL ), %%% to avoid circularity
+    
+    expand_current_node( (X0,Y0), ALL , Expanded_NODE),
+        
+    append( L_OPEN, Expanded_NODE, L ), %% NEIGHBOURG
+    heuristic_EVAL( L , L_Heur),
+    my_sort(L_Heur, L_Sorted),
+    
+    %% choice the best candidate
+    L_Sorted = [(Xbest,Ybest)/_| _ ], %%% by unification 
+    %%% 
+    
+    delete(L, (Xbest,Ybest), L_OPEN_NEW),   
+    
+    %% format("\n OPEN_NEW ~w\t L_Sorted \t ~w CLOSED ~w ", [L_OPEN_NEW, L_Sorted , L_CLOSED ]),
+    
+    search_BEST_FS(Xbest,Ybest,
+                   [(Xbest,Ybest),(X0,Y0)| L_CLOSED], 
+                   L_OPEN_NEW, SOL).
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% end of KERNEL 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%% expanding for immediate neighbours NOT VISITED YET
+%%% OPEN AND CLOSED .... to avoid circularity
+expand_current_node( (X0,Y0), ALL , L_out) :-
+   
+     findall( (X1,Y1)  , 
+      (  
+         d(X0,Y0,X1,Y1), %%% conexao
+         \+ member(  (X1,Y1) , [ (X0,Y0) | ALL] )
+       ), 
+      L_out ),
+      ! ,
+      format("\n [X0,Y0]: [~w,~w]", [X0,Y0]),
+      format("\n Expanded :: ~w", [L_out ])
+      .
+     
+
+%%% evalution functions -- Manhathan distance         
+heuristic_EVAL( [], [] ).   
+heuristic_EVAL([(X1,Y1)|L], [(X1,Y1)/H1 | L_H ]) :-
+        end_point(X_d,Y_d),
+        H1 is abs(X_d - X1) +    abs(Y_d - Y1),
+        heuristic_EVAL( L, L_H ).  
+             
+choice_the_best(L_H, (X_best,Y_best)) :-
+      min_value(L_H, (X_best,Y_best) ).
+
+my_sort([ ]):- format("\n SORT: this list is EMPTY"), !.
+my_sort([ T ] , [ T ] ). %% ONE VALUE
+%% TWO
+my_sort([(X1,Y1)/H1 , (X2,Y2)/H2], [(X1,Y1)/H1, (X2,Y2)/H2 ] ):-
+ H1 =< H2 . %% 
+%% TWO
+my_sort([(X1,Y1)/H1, (X2,Y2)/H2], [(X2,Y2)/H2,  (X1,Y1)/H1] ):-
+ H1 > H2 . %% 
+%% TWO
+my_sort([ H | L_1 ], [ MIN_UNKNOW | L_2] ) :- 
+      min_value( [ H | L_1 ] ,  MIN_UNKNOW),
+      delete([H | L_1], MIN_UNKNOW, L_res),
+      my_sort(L_res ,  L_2 ).
+
+      
+%%% FINDING THE MINIMAL HEURISTIC VALUE      
+
+min_value([ ], _ ):- format("\n this list is EMPTY"), !.
+
+min_value([ T ], T ). %% RETURNING HERE THE MINIMAL VALUE
+
+min_value([(X1,Y1)/H1, (X2,Y2)/H2 | L_H ], MIN_UNKNOW ) :- 
+      H1 =< H2, 
+      min_value([(X1,Y1)/H1 | L_H ], MIN_UNKNOW ). 
+      
+min_value([(X1,Y1)/H1, (X2,Y2)/H2 | L_H ], MIN_UNKNOW ) :- 
+      H1 > H2, 
+      min_value([(X2,Y2)/H2 | L_H ], MIN_UNKNOW ).
+    
 
 imp_lst([]).
 imp_lst([Cabeca|Cauda]):- 
@@ -118,6 +162,19 @@ my_min_test([H1 , H2 | L_H ], Min ) :-
       my_min_test([H2 | L_H ], Min).
 
 go :-my_min_test([3,4,1,6], X), print("\n X"), write(X).
+
+
+
+del_min_value( _ , [], []) :- !.
+del_min_value( (X1,Y1)/H1 , [ (X2,Y2)/H2 | L_1 ], [ (X2,Y2)/H2 | L_2 ]) :- 
+       H1 \== H2,
+       del_min_value((X1,Y1)/H1, L_1, [(X2,Y2)/H2 | L_2 ]).
+
+del_min_value( (X1,Y1)/H1 ,[ (X2,Y2)/H2 | L_1 ] , L_2 ) :- 
+       H1 \== H2,
+       del_min_value( (X1,Y1)/H1, L_1, L_2 ).
+       
+
 
 /*
 $ swipl -q -t main -f maze_DFS.pl 
