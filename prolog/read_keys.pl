97ff2c882de17e08b755c25fbfb667a22be78bb7
claudiosa <claudio.sa@udesc.br>
2019-09-18 01:28:08 -0300

    slides picat

---
+++ prolog/read_keys.pl
@@ -0,0 +1,34 @@
+
+
+%%with_tty_raw(:Goal)
+read_key([Code|Codes]) :-
+
+   get_single_char(Code),
+
+   read_pending_codes(user,Codes,[]).
+
+
+
+read_keyatom(KAtom) :-
+
+   read_key(Codes),
+
+   codes_keyatom(Codes,KAtom).
+
+
+
+codes_keyatom([27,91,65],up)    :- !.
+
+codes_keyatom([27,91,66],down)  :- !.
+
+codes_keyatom([27,91,67],right) :- !.
+
+codes_keyatom([27,91,68],left)  :- !.
+
+/*
+?- read_keyatom(Key).
+
+% Press the <up> key here
+
+Key = up.
+*/
