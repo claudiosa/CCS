2908e0a064335bd13d5599a35b74caab502ba3c2
claudiosa <claudio.sa@udesc.br>
2019-10-14 18:45:40 -0300

    slides picat

--- prolog/expert-system-stock-market/input_output_predicates.pl
+++ prolog/expert-system-stock-market/input_output_predicates.pl
@@ -1,3 +1,25 @@
+
+
+pause(X) :-
+    ttyflush,
+	get_single_char(X),
+	flush_output.
+
+
+read_option(Y_LowerCase) :- 
+  flush_output,
+  get_single_char(Y) , %% works fine
+  atom_char(X,Y) ,     %% OK
+  downcase_atom(X, Y_LowerCase), %% convert to Lower_Case
+  member(Y_LowerCase, ['0','1', '2', '3', '4', '5', '6', '7', '8','9' ]),
+  format("\n Keypressed =>  (key: ~w) (ascii code: ~w) ", [X,Y] ),!.
+
+read_option(Keypressed) :-
+  format("\n Please, press something from 0 up to 9 ", [] ),
+  get_single_char(Keypressed).
+
+
+
 read_valid_key(Y_LowerCase) :- 
   flush_output,
   get_single_char(Y) , %% works fine
@@ -11,7 +33,7 @@ read_valid_key(Y_LowerCase) :-
 %% tty_clear
 cls :- write('\e[H\e[2J').
 
-pause :- get_single_char(_).	
+%pause :- get_single_char(_).	
 
 /*
 upcase_atom(+AnyCase, -UpperCase)
--- prolog/expert-system-stock-market/kb.pl
+++ prolog/expert-system-stock-market/kb.pl
@@ -3,3 +3,42 @@
 %%%%     knowledge base      %%%%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % begin k-base :
+
+%%% 7 variables or atributes
+atributes([month, open, high, low, close, volume, inflation_rate]).
+
+%%% values of each variables
+atrib_values(month,[previous_month, 6_month_ago]).
+atrib_values(open,[is_rising, at_same, is_going_down]).
+atrib_values(high,[high, at_same, low]).
+atrib_values(low,[high, at_same, low]).
+atrib_values(close,[is_rising, at_same, is_going_down]).
+atrib_values(volume,[high, at_same, low]).
+atrib_values(inflation_rate,[increases, at_same, decreases]).
+
+/*
+Rule 9: IF inflation rate decreases then share price also
+decreases that shows the recession period of country.
+*/
+rule(if_([inflation_rate/decreases]), then_('share price also
+decreases that shows the recession period of country')).
+
+/*
+Rule 10: IF close price of share is rising and check also open
+price and high price also rising with the same ratio then sell
+share and earn money
+*/
+
+rule(if_([close/is_rising, open/is_rising, high/is_rising, inflation_rate/at_same],
+     then_('sell share and earn money'). 
+
+
+
+
+
+
+
+
+
+
+
