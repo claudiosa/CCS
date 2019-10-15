56d0e83fa0dc8ef001c5999c423ed84a65f66472
claudiosa <claudio.sa@udesc.br>
2019-10-15 01:00:40 -0300

    slides picat

--- prolog/expert-system-stock-market/input_output_predicates.pl
+++ prolog/expert-system-stock-market/input_output_predicates.pl
@@ -1,37 +1,44 @@
 
-
-pause(X) :-
-    ttyflush,
-	get_single_char(X),
-	flush_output.
-
-
 read_option(Y_LowerCase) :- 
   flush_output,
   get_single_char(Y) , %% works fine
   atom_char(X,Y) ,     %% OK
   downcase_atom(X, Y_LowerCase), %% convert to Lower_Case
   member(Y_LowerCase, ['0','1', '2', '3', '4', '5', '6', '7', '8','9' ]),
-  format("\n Keypressed =>  (key: ~w) (ascii code: ~w) ", [X,Y] ),!.
+  format("\n Keypressed =>  (key: ~w) (ascii code: ~w) ", [X,Y] ),
+  write(Y_LowerCase), ! .
 
 read_option(Keypressed) :-
   format("\n Please, press something from 0 up to 9 ", [] ),
   get_single_char(Keypressed).
 
-
-
-read_valid_key(Y_LowerCase) :- 
-  flush_output,
-  get_single_char(Y) , %% works fine
-  atom_char(X,Y) ,     %% OK
-  downcase_atom(X, Y_LowerCase), %% convert to Lower_Case
-
-  member(Y_LowerCase,['a','b','c','d','x' ]),
-  format("\n Keypressed =>  (key: ~w) (ascii code: ~w) ", [X,Y] ).
-
+%%% show the options of each attribute
+print_L_values([]):- nl.
+print_L_values([First|Rest]) :-
+ %   format("\n => ~w", First),
+     ansi_format([bold,fg(yellow)],'\n => ~w', [First]),
+     print_L_values(Rest). 
+  
+%%%% without syntax verification
+%read_Value([First|Rest], Value) :-
+read_Value( Value ) :-
+    ansi_format([bold,fg(red)],'\n Experimental Reading ...', []),
+    format("\n EXACTLY as written ABOVE =>  "),
+    read_line_to_codes(user_input, VarCodes),
+    atom_codes(Value, VarCodes).
+    
+    %member(Value,[First|Rest]),
 
 %% tty_clear
 cls :- write('\e[H\e[2J').
+
+
+pause(X) :-
+  ttyflush,
+  get_single_char(X),
+  flush_output.
+
+
 
 %pause :- get_single_char(_).	
 
--- prolog/expert-system-stock-market/kb.pl
+++ prolog/expert-system-stock-market/kb.pl
@@ -8,13 +8,20 @@
 atributes([month, open, high, low, close, volume, inflation_rate]).
 
 %%% values of each variables
-atrib_values(month,[previous_month, 6_month_ago]).
+%atrib_values(month,[previous_month, six_month_ago]). %%
+atrib_values(month,[bbb,aa,ccd]) . %%
 atrib_values(open,[is_rising, at_same, is_going_down]).
 atrib_values(high,[high, at_same, low]).
 atrib_values(low,[high, at_same, low]).
 atrib_values(close,[is_rising, at_same, is_going_down]).
 atrib_values(volume,[high, at_same, low]).
 atrib_values(inflation_rate,[increases, at_same, decreases]).
+
+%%% FAKE RULEs
+rule(if_([month/bbb] ), then_('yyyyyyyyyyyyyyyyyy')).
+rule(if_([month/aa] ), then_('xxxxxxxxxxxxxxxxxx')).
+
+
 
 /*
 Rule 9: IF inflation rate decreases then share price also
@@ -28,15 +35,19 @@ Rule 10: IF close price of share is rising and check also open
 price and high price also rising with the same ratio then sell
 share and earn money
 */
-
-rule(if_([close/is_rising, open/is_rising, high/is_rising, inflation_rate/at_same],
-     then_('sell share and earn money'). 
-
+rule(if_([close/is_rising, open/is_rising, high/is_rising, inflation_rate/at_same]),
+     then_('sell share and earn money')). 
 
 
 
 
 
+%%% very trick ... atom_chars
+collecting_data(L) :-
+  findall(X/Y, if_(X,Y), L),!. 
+collecting_data([]). %%% exception
+
+ %findall(X/A, (if_(X,Y), atom_chars(A,[Y])), L). 
 
 
 
