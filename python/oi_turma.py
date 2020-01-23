61c5f22b2e8df3583940b697fbf0368afef34cc6
claudiosa <claudio.sa@udesc.br>
2020-01-23 15:43:29 -0300

    comitado do VSCODE

--- python/oi_turma.py
+++ python/oi_turma.py
@@ -1,5 +1,10 @@
 
-print("  Oi turma de ALP" );
-print("  Wellcome .... 2018");
-print("  Linguagem Python => TOP ");
+print("  Oi turma de ALP" )
+print("  Wellcome .... 2018")
+print("  Linguagem Python => TOP ")
+### isto comenta uma linha
+'''
+um bloco ...
+e não precisa de ; no final do comando....
 	
+'''
