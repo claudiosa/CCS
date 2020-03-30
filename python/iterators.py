b9d06563fe63dd19ad02f317677d81ad2977205a
claudiosa <ccs1664@gmail.com>
2020-03-30 13:25:31 -0300

    shortest OK

--- python/iterators.py
+++ python/iterators.py
@@ -1,6 +1,15 @@
 
 
 ## all  except ...
-k = [x for x in range(90) if ((x != 5) and x < 19)]
-for i in k:
+'''
+k1 = [x for x in range(90) if ((x != 5) and x < 19)]
+for i in k1:
     print(f'-> %i' %i)
+'''
+## 5 e 15 não inclusos
+k2 = [x for x in range(20) if ((x != 5) and (x != 15)) ]    
+for i in k2:
+    print(f'-> %i' %i)
+
+v = [3,4,5]
+print(len(v))
