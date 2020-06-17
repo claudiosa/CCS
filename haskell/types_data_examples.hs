e42f7f2148636e93a833ae55717af6f51dcccb0b
claudiosa <ccs1664@gmail.com>
2020-06-17 11:09:41 -0300

    types

--- haskell/types_data_examples.hs
+++ haskell/types_data_examples.hs
@@ -1,22 +1,29 @@
 -- Algebraic data types
 -- data Bool = False | True  > from Prelude.hs
-data My_Const_Bool = My_Bool Bool
+
+type My_ZERO_or_ONE = Int
+data My_Const_Bool = Val_BOOL My_ZERO_or_ONE
     deriving (Show) 
 
--- my_true :: My_Bool 
-my_true = True
+my_true :: My_Const_Bool
+my_true = Val_BOOL 1
+{-
+*Main> :t my_true
+my_true :: My_Const_Bool
+-}
 
-data Constructor_Car = Car 
+data Type_Const_Car = Car 
     {company :: String, 
      model :: String, 
      year :: Int} 
      deriving (Show)
 
-car_01 :: Constructor_Car
+-- Car isolated is the value constructor or data constructor
+car_01 :: Type_Const_Car
 car_01 =  Car "Ford" "Mustang" 1967                            --
-car_02 :: Constructor_Car
+car_02 :: Type_Const_Car
 car_02 =  Car "BMW" "X5566" 2020                            --
 --  {company="Ford", model="Mustang", year=1967} 
 
-vector_of_car :: [Constructor_Car]                               --
+vector_of_car :: [Type_Const_Car]                               --
 vector_of_car = [car_01, car_02]                               --
