619be20910b08aaa023cebc8dc5e8d57b874db1b
claudiosa <ccs1664@gmail.com>
2020-09-29 11:06:14 -0300

    minors corrections

--- haskell/some_f_lists.hs
+++ haskell/some_f_lists.hs
@@ -1,5 +1,10 @@
-n_esimo  _ [] = error "EMPTY LIST"
-n_esimo n _ | n <= 0 = error "negative number or invalid index"
+
+
+module MY_LISTS where
+
+{-  To be improved -}
+n_esimo  _ [] = error "EMPTY LIST" 
+n_esimo n _ | n <= 0 = error "Negative number or invalid index"
 n_esimo 1 (a:_) = a
 n_esimo n (a:b) = n_esimo (n-1) b
 {-
@@ -20,13 +25,14 @@ CallStack (from HasCallStack):
 *Main> 
 
 -}
+{-  To be improved -}
 back_index_of_value  _ [] = error "X is not in this list"
 back_index_of_value  x (a:_) | x ==a  = 1
 back_index_of_value  x (a:b) | not(x == a) = 1 + back_index_of_value x b
 back_index_of_value _ _   = error "Please check your code"
 
 
-{-fun��o para verificar se o n�mero � primo-}
+{- prime number -}
 eh_primo :: Int -> Bool
 eh_primo 0  =  False
 eh_primo 1  =  True
@@ -34,15 +40,14 @@ eh_primo n
         |(n_divisiveis n n == 2)   = True
         | otherwise         = False
 
-{-fun��o que encontra por quantos n�meros o n�mero passado como
-par�metro � divis�vel. -}
+{- how many divisors between two numbers a/b -}
 n_divisiveis :: Int-> Int-> Int
 n_divisiveis a 1   = 1
 n_divisiveis a b
         |(mod a b == 0) = 1 + n_divisiveis a (b-1)
         |otherwise      = n_divisiveis a (b-1)
 
-{-fun��o que encontra o primeiro n�mero primo na lista-}
+{- given a list, which is the first prime number -}
 primeiro_primo :: [Int] -> Int
 primeiro_primo (x:xs) | eh_primo x = x
         | otherwise = primeiro_primo xs
