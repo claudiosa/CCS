4905c9df1e07ab7545da7ca45a8c1f6d0c39bc75
claudiosa <ccs1664@gmail.com>
2020-10-29 17:42:39 -0300

    sources ...

--- bash_scripts_cc/samsung_a_direita.sh
+++ bash_scripts_cc/samsung_a_direita.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-### xrandr -q  %%% ver os nomes dos monitores reconhecidos  --left-of LVDS-0
-  xrandr --output eDP1 --primary --mode 1366x768 --pos 0x0 --rotate normal --output DP1 --mode 1366x768 --pos 1366x0 --rotate normal --output HDMI1 --off --output VIRTUAL1 --off
-echo "monitores OK ...."
+xrandr --output LVDS-1 --mode 1366x768 --pos 0x0 --rotate normal --output DP-1 --off --output HDMI-1 --off --output VGA-1 --mode 1366x768 --pos 1366x0 --rotate normal
+echo "OK via arandr"
