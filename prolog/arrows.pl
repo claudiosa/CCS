8126b2b53b8e2dec83c0603e593bc180f0a2940d
claudiosa <ccs1664@gmail.com>
2020-11-19 20:33:52 -0300

    sources ...

---
+++ prolog/arrows.pl
@@ -0,0 +1,9 @@
+%%%On my side (Windows 10 + SWI Prolog 8.3.11), it works with:
+
+codes_keyatom([16],up) :- !.
+
+codes_keyatom([14],down) :- !.
+
+codes_keyatom([6],right) :- !.
+
+codes_keyatom([2],left)  :- !.
