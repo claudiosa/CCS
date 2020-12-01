6e4cecba4e93d2d572b362434fe9b9d28cbe7044
claudiosa <ccs1664@gmail.com>
2020-12-01 19:51:16 -0300

    sources ...

---
+++ julia/01_lesson.jl
@@ -0,0 +1,13 @@
+#=
+$ julia 01_lesson.jl 
+Hello World
+=#
+
+println("Hello World");
+print("GeeksForGeeks") 
+
+function test()
+	println("\n\nIN test function");
+end
+
+test()
---
+++ julia/02_lesson.jl
@@ -0,0 +1,10 @@
+
+# Defining a function with arguments 
+function add_fn(x, y) 
+    print("\n\nThe output is: ");
+    println(x + y) 
+end 
+  
+# Calling defined function 
+add_fn(10, 8) 
+
