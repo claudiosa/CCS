84254e421953c24a847f14d8a6b4aaf8896de0a5
claudiosa <ccs1664@gmail.com>
2021-04-10 21:41:55 -0300

    sources ...

--- haskell/constraint_problems/myproj/app/Main.hs
+++ haskell/constraint_problems/myproj/app/Main.hs
@@ -1,14 +1,15 @@
 {-# OPTIONS_GHC -Wall -Werror #-}
--- module Main where
--- import Lib
 
 --module  Documentation.SBV.Examples.Puzzles.SendMoreMoney where
 module Main where
+-- import  Send_more_money
 import Data.SBV
-
+-- https://hackage.haskell.org/package/sbv-8.14/docs/src/
 main :: IO ()
-main = print sendMoreMoney
-
+-- main = print =<< sendMoreMoney
+main = do
+  result <- send_2 -- sendMoreMoney
+  print result
 
 -- | Solve the puzzle. We have:
 --
@@ -28,6 +29,7 @@ main = print sendMoreMoney
 --
 -- >>> 9567 + 1085 == 10652
 -- True
+-- OK
 sendMoreMoney :: IO AllSatResult
 sendMoreMoney = allSat $ do
         ds@[s,e,n,d,m,o,r,y] <- mapM sInteger ["s", "e", "n", "d", "m", "o", "r", "y"]
@@ -40,3 +42,28 @@ sendMoreMoney = allSat $ do
         constrain $ distinct ds
         constrain $ s ./= 0 .&& m ./= 0
         solve [send + more .== money]
+
+
+
+send_2 :: IO AllSatResult
+send_2 = allSat $ do
+        [s,e,n,d,m,o,r,y] <- mapM sInteger ["s", "e", "n", "d", "m", "o", "r", "y"]
+        let 
+        --isDigit x = x .>= 0 .&& x .<= 9
+        for_each [s,e,n,d,m,o,r,y]
+        val send =  s*1000 + e*100 + n*10 + d
+        val more =  m*1000 + o*100 + r*10 + e 
+        val money = m*10000 + o*1000 + n*100 + e*10 + y 
+
+        constrain $ distinct [s,e,n,d,m,o,r,y]
+        constrain $ sAll isDigit [s,e,n,d,m,o,r,y]
+        
+        constrain $ s ./= 0 .&& m ./= 0
+        solve [send + more .== money]
+
+
+for_each [] = True
+for_each (a:b) = 
+     (a .>= 0 .&& a .<= 9) : for_each b
+
+
