6fc3d1bdab24cdaa52e5fbdfa176392ea175b097
claudiosa <ccs1664@gmail.com>
2021-04-16 17:43:00 -0300

    node.js

---
+++ react.js/01-project/src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div class="teste"> 
+      <center>Learning ... </center>
+      <h1> <center> Hello World !!!! 
+    <p>===============</p>
+     Finally!!!!</center>    </h1>
+     fasdlkfasj   <br/>
+     adfjalf  <br/>
+     dakfalj  <br/>
+      xxxxx
+    </div>
+        
+    );  
+  }
+export default App;    
