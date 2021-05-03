d5a3826671f67651d9488160281c83157165247f
claudiosa <ccs1664@gmail.com>
2021-05-03 18:29:01 -0300

    functions in js

---
+++ javascript/01_functions.js
@@ -0,0 +1,28 @@
+
+
+function junta_Atomos( a, b){
+    return('Junta palavras ... ' + a + b)
+}
+
+const total = junta_Atomos('palavra_01', 'word_02')
+
+console.log(`TOP  de total:  ${total}`)
+
+
+
+
+
+
+const sum = function(a,b){ 
+    // let a = xxxx
+    // console.log(a+b)
+    return(a + b) 
+   }
+
+
+sum(2,4) // calling again ....
+
+console.log(' Another  call: ' , sum(3,4) )    
+console.log(` Another  call:  ${sum(3,4)}` ) 
+
+console.log('The End of Code ')
