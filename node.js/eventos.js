e5dad7154c7caa3e6b6ce41b3924d4ee28584621
Claudio Cesar de Sá <ccs1664@gmail.com>
2021-09-14 17:12:49 -0300

    updates

--- node.js/eventos.js
+++ node.js/eventos.js
@@ -11,12 +11,16 @@ function f_log(PARAM_f_log ) {
     emissor.emit('f_log' , PARAM_f_log);
 }
 
+//Exportando esta função -- pronta para ser usado em outros pgms
+module.exports = f_log
+
+
 // with error YET
 // function f_log_arrow(  (PARAM_f_log)  => {
 //    emissor.emit('f_log_arrow' , PARAM_f_log);
 // })
 
 
-f_log(" algo");
-f_log(" aqui");
-f_log("e lá");
+//f_log(" algo");
+//f_log(" aqui");
+//f_log("e lá");
--- node.js/mostra_MEM.js
+++ node.js/mostra_MEM.js
@@ -4,7 +4,14 @@
 // importando um módulo
 const VAR_os = require('os');
 
+//importando um user-module
+const f_log = require('./eventos'); //./ : pasta local ou corrent
+
+
+
+
 // uma função global
+
 setInterval( () => {
     
     const { freemem, totalmem } = VAR_os
@@ -20,7 +27,28 @@ setInterval( () => {
     }
     
     console.clear();
-    console.log("=========== STATUS ================");
+    //console.log("=========== STATUS ================");
+    f_log("=========== STATUS ================");
+    // prints time in HH:MM format
+    //console.log(hours + ":" + minutes + ":" + seconds);
+    f_log(get_my_time());
     console.table(status);
+
+    //já formatando numa saída json
+    f_log(` ${JSON.stringify(status)} \n `) ; // 1 linha por passada.
+
+
+
     }, 1000)
 
+
+function get_my_time() {
+    let date_object = new Date();
+    // current hours
+    let hours = date_object.getHours();
+    // current minutes
+    let minutes = date_object.getMinutes();
+    // current seconds
+    let seconds = date_object.getSeconds();
+    return(hours + "h:" + minutes + "m:" + seconds+"s");
+}
