41943af0263385aa9da89c516f252af7f301d4ac
Claudio Cesar de Sá <ccs1664@gmail.com>
2021-10-29 18:24:37 -0300

    calculadora

---
+++ javascript/calculadora.js
@@ -0,0 +1,19 @@
+function soma(a,b) {
+ 
+    var z =   parseFloat(a) +  parseFloat(b) ; // NUMBERS
+    console.log('Passou em SOMA: ', z);
+    document.write ( z );
+    alert(z);
+    return (z)
+  }
+
+
+  function DisplayFunction() {
+    var result;
+    result = concatenate('John ', 20) ;
+    document.write (result );
+ }
+
+
+
+     
