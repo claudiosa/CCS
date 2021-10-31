036a8cbc68b34be4b4975c6d2f9554deb0e278b9
claudiosa <ccs1664@gmail.com>
2021-10-31 19:38:25 -0300

    asp

---
+++ prolog/nick_reyntjens/example_data_set.pl
@@ -0,0 +1,29 @@
+%%% LEXICOGRAPHY ORDER ....
+%%% Follow that notation in a sequence of numbers.
+%%% records ...= fact - dynamics
+%%% FUNCTOR 
+
+rec(dataset("lawfirm 1",  
+    agents("Ann",100, "Ann@lawfirm1.com", 8, no, 
+           patent("thootbrush",8,no, 
+             task("write description", 5))))).
+
+rec(dataset("lawfirm 1",  
+    agents("Ann",100, "Ann@lawfirm1.com", 8, no, 
+           patent_done("chair",4, yes, 
+             task("review description", 5))))).
+
+
+
+rec(dataset("lawfirm 1",  
+    agents("Ann",100, "Ann@lawfirm1.com", 8, no, 
+           patent_most_est_time("spreadsheet",14, no, 
+             )))).
+
+
+
+
+rec(dataset("lawfirm 2",  
+    agents("Mark",300, "Mark@lawfirm2.com", 9, 4200, 
+           patent("database",6,no, 
+             task("write description", 3))))).
