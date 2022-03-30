9db35cd9b9d12176c4340f2b812f7d414dd33652
claudiosa <ccs1664@gmail.com>
2022-03-29 21:06:01 -0300

    Update maps_study.v and random_study.v

--- v_programming_language/maps_study.v
+++ v_programming_language/maps_study.v
@@ -1,4 +1,5 @@
 import rand
+import math    // for abs
 
 /*
 Exploring maps and functions
@@ -18,7 +19,7 @@ fn my_ugly_sum (an_array[] int) int {
 	mut n := an_array.len
 	mut s := 0
 	for i in 0 .. n {
-		s = s + an_array[i]
+		s +=  an_array[i] // shorted notation
 	}
 	return s
 }
@@ -32,8 +33,9 @@ fn creating_a_map (size i16)  map [string] i32 {
 
 	for _ in 0 .. size {
 		//my_map[key] = math.abs(rand.i16()  % 128)  // a random from -128 ... 127
-		my_map[key] = rand.i64() % 128   // a random from -128 ... 127 
-		//my_map[key] =  ${rand.intn(size) ?}  UNCOMPLETE
+		//my_map[key] = rand.i64() % 128   // a random from -128 ... 127 
+		//my_map[key] =  i32(rand.intn(size)) //?}  UNCOMPLETE
+		my_map[key] =  math.abs(rand.i16()  % size)  
 		key = key + the_b + the_a // concat string
 	}
     // print('\n My map in the function: ${my_map}' )
@@ -41,14 +43,14 @@ fn creating_a_map (size i16)  map [string] i32 {
  }
 
 // receving and printing a map
-fn printing_a_map ( my_map map [string] i32) {
+fn printing_a_map ( my_map map [string] i32)  {
 	mut array_of_keys := my_map.keys()
 	// println(m.keys()) // ['one', 'two']
 	println("\n Printing a map:")
 	for i in array_of_keys {
-	  print('\n the key[${i}] has the contents:  ${my_map[i]} ' )
+	  print('\n the key: [${i}] has the contents: ${my_map[i]} ' )
 	}
-    //print('\n My map in the function: ${array_of_keys}' )
+	//print('\n My map in the function: ${array_of_keys}' )
  }
 
 
@@ -57,7 +59,8 @@ fn main() {
     // mut my_map := map [string] int{} OK
 	// mut my_map := map [string] i16{} //OK
 	print('\n Creating map from a function: ${creating_a_map(10)}' )
-	printing_a_map(creating_a_map(4))
+	
+	printing_a_map( creating_a_map(7) )
     my_hello() 
 	print('\n The ugly sum: ${ my_ugly_sum([4,5,6,7,8,9]) }'  )
 	mut my_map_2 := map [string] i16{} //OK
--- v_programming_language/random_study.v
+++ v_programming_language/random_study.v
@@ -12,17 +12,25 @@ $ ./an_executable.EXE
 
 fn main() {	
 	an_array := ['check', 'whether', 'contains', 'an', 'element']
-	test := [rand.i64_in_range(0,64) ?]
-	println('A number: ${test}')
-	print('Guessing what:  ${test} \n'  )
-	println('A number: ${test[0]}')
+	test1 := [rand.i64_in_range(0,64) ?]
+	test2 := [ rand.intn(69) ?]
+	test3 := ( rand.intn(69) )
+	x := ( rand.i64_in_range(0,64) ) 
+	println('x: ${x}')
+	
+	//test3 := (rand.string_from_set({'ax': 'bx': 'cx': } , 2)  ?)
+	
+	println('A number: ${test1} and ${test2} and ${test3}')
+	print('Guessing what:  ${test1} '  )
+	println('A number: ${test1[0]} and ${test2[0]}')
 	println('A number: ${rand.i64() % 69}')
+	println('A number: ${rand.intn(69)}'  )
 	println('A number: ${rand.i64_in_range(0,64)}') // Option(.....)
 	println('A number: ${rand.int_in_range(0,64)}') 
 	println('A number: ${rand.int_in_range(0,an_array.len) ? }')
 	println('A number: ${an_array[rand.intn(an_array.len) ? ]}')
 
-    println('A string: ${rand.string_from_set({'a','b','c'}, 2) ?}' )
+   /* println('A string: ${rand.string_from_set({'a','b','c'} , 2) ?}' )*/
 
 
 	println('\n BYE')
