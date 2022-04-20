7b94cc4d583a0beeef100672d30e226076a2b591
claudiosa <ccs1664@gmail.com>
2022-04-20 19:17:33 -0300

    Update bfs_fila.c, dfs_pilha.c, and 6 more files...

--- v_programming_language/a_DFS.v
+++ v_programming_language/a_DFS.v
@@ -1,32 +1,36 @@
-//   NOT READY YET
+//
 // Depth-First Search (BFS) allows you to ﬁnd a path between two nodes in the graph.
 // I  follow literally code in C, done many years ago
-
+// coded by CCS
+//
 fn depth_first_search_path(graph map[string][]string, start string, target string) []string 
  {
 	mut path := []string{} // ONE PATH with SUCCESS = array
-	mut stack := []string{} // a stack ... many paths
+	mut stack := []string{} // a stack ... many nodes 
 	//all_nodes := graph.keys() // get a key of this map
 	n_nodes := graph.len // numbers of nodes of this graph
 	mut visited := a_map_nodes_bool (n_nodes) // a map fully
 	//false ... not visited yet: {'A': false, 'B': false, 'C': false, 'D': false, 'E': false}
 	
-	stack.prepend(start) // first arrival .... prepend = PUSH on the stack
-	for (stack.len != 0) // or stack.len > 0
-	{
+	stack.prepend(start) // first push .... prepend = PUSH on the stack
+	for (stack.len != 0) // or  WHILE (stack.len > 0)
+	{  //a pull from the stack
        mut node := get_from_top( mut stack ) // get the front node and remove it
 	  
 	   if  visited[ node ] == false
 		{// check if this node is already visited
-		  // if no ... test it searchinf for a final node
-		  visited[node] = true // means: visit this node
+		  // if no ... test it searchin for a final node
+		  visited[node] = true // means: node visited
 		  if node == target {
 		    path = build_path_reverse(graph, start, node, visited)
 			return path
 		}
-	// expansion of node removed from  stack
-	print("\n Expansion of node ${node} (true/false): ${graph[node]}")
-	for vertex in graph[node]  // take  all nodes from the node
+	//  Exploring of node removed from  stack and add its relatives
+	print("\n Exploring of node ${node} (true/false): ${graph[node]}")
+	//graph[node].reverse() take a classical choice for DFS
+	// at most os left in this case. 
+	// use vertex in graph[node] the choice is right
+	for vertex in graph[node].reverse()  // take  all nodes from the node
 		{
 		  //println("\n ...${vertex}")	
           if visited[vertex] == false // not explored yet
@@ -34,7 +38,7 @@ fn depth_first_search_path(graph map[string][]string, start string, target strin
            stack.prepend( vertex )
 		  }
 		}
-		print("\n stack: ${stack} (only not visited) \n Visited: ${visited}")
+		print("\n Stack: ${stack} (only not visited) \n Visited: ${visited}")
 		}
 	} 
 	path = ['Path not found, problem in the Graph, start or end nodes! ']
@@ -43,20 +47,35 @@ fn depth_first_search_path(graph map[string][]string, start string, target strin
 
 fn main() {
 // Adjacency matrix as a map	
-	graph := {
+//Example 01
+	graph_01 := {
 		'A': ['B', 'C']
-		'B': ['A', 'D'] //, 'E']
+		'B': ['A', 'D', 'E']
 		'C': ['A', 'F']
 		'D': ['B']
-		'E': ['F']      //['B', 'F']
+		'E': ['F', 'B', 'F']
 		'F': ['C', 'E']
 	}
-	println('Graph: $graph')
-	path := depth_first_search_path(graph, 'A', 'D')
-	println('\n A first path from node A to node F is: ${path.reverse()}')
+	//Example 02
+	graph_02 := {
+		'A': ['B', 'C','D']
+		'B': ['E']
+		'C': ['F']
+		'D': ['E']
+		'E': ['H']
+		'F': ['H']
+		'G': ['H']
+		'H':  ['E', 'F','G']
+	}
+	//println('Graph: $graph')
+	path_01 := depth_first_search_path(graph_01, 'A', 'F')
+	println('\n Graph_01: a first path from node A to node F is: ${path_01.reverse()}')
+	path_02 := depth_first_search_path(graph_02, 'A', 'H')
+	println('\n Graph_02: a first path from node A to node F is: ${path_02.reverse()}')
+
 }
-//////////// AUX FUNCTIONS //////////////////
-// Creating a map for VISITED nodes ... 
+//////////// AUXILIAR FUNCTIONS //////////////////
+// Creating a map for nodes not VISITED visited ... 
 // starting by false ===> means this node 
 // was not visited yet
 fn a_map_nodes_bool (size int)  map [string]  bool {
@@ -71,7 +90,6 @@ fn a_map_nodes_bool (size int)  map [string]  bool {
  }
 
 // classical a departure node  from stack
-
 fn get_from_top(mut a_stack [] string) string{
    mut x := a_stack.first() //a.first() equivalent to a[0]
    //REMOVE IT
@@ -92,43 +110,19 @@ path << current
 
 for ( current != start)
 {
-  	for i in array_of_nodes 
+ 	for i in array_of_nodes 
 	{
 	 if (current in graph[i]) && (visited[i]==true)
 	   { current = i
 		 break // the first ocurrence is enough
 	   }
 	}
- path << current //update the path tracked
+ 	path << current //updating the path tracked
 }
 return path
 }
 
 /*
-$ v run another_BFS.v 
-Graph: {'A': ['B', 'C'], 'B': ['A', 'D', 'E'], 'C': ['A', 'F'], 'D': ['B'], 'E': ['B', 'F'], 'F': ['C', 'E']}
-
- Expansion of node A (true/false): ['B', 'C']
- stack: ['B', 'C'] (only not visited) 
- Visited: {'A': true, 'B': false, 'C': false, 'D': false, 'E': false, 'F': false}
- Expansion of node B (true/false): ['A', 'D', 'E']
- stack: ['C', 'D', 'E'] (only not visited) 
- Visited: {'A': true, 'B': true, 'C': false, 'D': false, 'E': false, 'F': false}
- Expansion of node C (true/false): ['A', 'F']
- stack: ['D', 'E', 'F'] (only not visited) 
- Visited: {'A': true, 'B': true, 'C': true, 'D': false, 'E': false, 'F': false}
- Expansion of node D (true/false): ['B']
- stack: ['E', 'F'] (only not visited) 
- Visited: {'A': true, 'B': true, 'C': true, 'D': true, 'E': false, 'F': false}
- Expansion of node E (true/false): ['B', 'F']
- stack: ['F', 'F'] (only not visited) 
- Visited: {'A': true, 'B': true, 'C': true, 'D': true, 'E': true, 'F': false}
-
- Nodes visited (true) or no (false): {'A': true, 'B': true, 'C': true, 'D': true, 'E': true, 'F': true}
- The shortest path from node A to node F is: ['A', 'C', 'F']
-[ccs@vosges v_programming_language]$ 
-
-
 
 SOME TESTS
 >>> mut stack := []int{}
@@ -146,7 +140,4 @@ SOME TESTS
 >>> println(graph['F'])
 ['C', 'E']
  
- 
- 
  */
-
