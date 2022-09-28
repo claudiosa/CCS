65217f44bf2c7412c07775a677b2aec008e5e1c8
claudiosa <ccs1664@gmail.com>
2022-09-28 15:02:41 -0300

    Update backup_sync.sh

--- bash_scripts/backup_sync.sh
+++ bash_scripts/backup_sync.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 rsync   -avzrul   --delete Dropbox/ /media/ccs/hd_externo/Dropbox/
 rsync   -avzrul   --delete principal/ /media/ccs/hd_externo/principal/
+# ANOTHER EXAMPLE
+# rsync -avru --ignore-existing /media/ccs/FAT32/todas_fotos/* /media/ccs/19B6-A4561/todas_fotos/
+# add --progress
 echo "Tudo sincronizado !!!"
