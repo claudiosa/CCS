43ccbdc435e0a5e5d328ee2a76de9d3bd5e1f9bc
claudiosa <ccs1664@gmail.com>
2022-10-06 20:51:26 -0300

    xxx

---
+++ python/turing_test/01_sol_turing.py
@@ -0,0 +1,25 @@
+from os import remove
+import sys
+# print(sys.argv)
+
+
+def calPoints(ops) -> int:
+    """
+    :type ops: List[str] - List of operations
+    :rtype: int - Sum of scores after performing all operations
+    """
+    result = None
+
+    return result
+
+
+if __name__ == '__main__':
+    print(sys.argv)
+    line = sys.argv.pop(0)  # remove the first element
+#    line = (sys.argv).remove(str(header))
+
+    print("\n the input string: ", line)
+    #line = input()
+    ops = line.strip().split()
+    print(ops)
+#        print(calPoints(ops))
---
+++ python/turing_test/sol.c++
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+#include<stack>
+#include <sstream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+  public:
+…    items.push_back(item);
+  }
+
+  Solution solution;
+
+  cout << solution.calPoints(items);
+}
