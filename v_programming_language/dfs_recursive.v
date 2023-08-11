b5cca74cab8b93db9e0f9854c5b0b513057dda21
claudiosa <ccs1664@gmail.com>
2023-08-10 22:32:30 -0300

    modified:   v_programming_language/dfs_recursive.v

--- v_programming_language/dfs_recursive.v
+++ v_programming_language/dfs_recursive.v
@@ -25,7 +25,7 @@ procedure DFS(G, v) is
 
 OR
 
-
+https://favtutor.com/blogs/depth-first-search-python#:~:text=The%20Depth-First%20Search%20is,if%20potential%2C%20else%20by%20backtracking.
 def dfs(visited, graph, node):  #function for dfs 
     if node not in visited:
         print (node)
@@ -50,8 +50,8 @@ fn main() {
 		'E': ['F', 'B', 'F']
 		'F': ['C', 'E']
 	}
+
 	// Example 02
-	/*
 	graph_02 := {
 		'A': ['B', 'C', 'D']
 		'B': ['E']
@@ -62,89 +62,76 @@ fn main() {
 		'G': ['H']
 		'H': ['E', 'F', 'G']
 	}
-	*/
+
+	// Example 03
+	graph_03 := {
+  	'5' : ['3','7'],
+  	'3' : ['2', '4'],
+  	'7' : ['8'],
+  	'2' : [],
+  	'4' : ['8'],
+  	'8' : []
+	}
+		
 	// println('Graph: $graph')
-	mut visited := visited_init(graph_01) // a map fully with false in all vertex
+	mut visited := visited_init( graph_01 ) // a map fully with false in all vertex
 	// false ... not visited yet: {'A': false, 'B': false, 'C': false, 'D': false, 'E': false}
 	dfs_recursive(mut visited, graph_01, 'A', 'E')
-	//println('\n Graph_01: a first path from node A to node F is: $path_01.reverse()')
-	//path_02 := depth_first_search_path(graph_02, 'A', 'H')
-	//println('\n Graph_02: a first path from node A to node F is: $path_02.reverse()')
+	
+	visited = visited_init( graph_02 ) // a map fully with false in all vertex
+	dfs_recursive(mut visited, graph_02, 'A', 'H')
+	
+	visited = visited_init( graph_03 ) // a map fully with false in all vertex
+	dfs_recursive(mut visited, graph_03, '5', '8')
 }
 
 // Depth-First Search (BFS) allows you to find a path between two nodes in the graph.
-fn dfs_recursive(mut visited map [string] bool,   graph map[string][]string,  current string, target string)  {
-	
+fn dfs_recursive(mut visited map [string] bool,  graph map[string][]string,  current string, target string)  {
 
 	if visited[current] == false {
-		visited[current] = true
-		print('\n Visiting: ${current} \t Target: ${target}')
+    	visited[current] = true
+		//print('\t Visiting: ${current} ')
+		print(' => ${current} ||')
 		if current == target 
 		{ 
-			print('\n FOUND IT')
-			return 
+			print('\n The node target was found:  ${target} \n')
+			//exit(1)
 		}
 	}	
-	mut adjacent_nodes_not_visited := []string{}
-	adjacent_nodes_not_visited = neighbours_not_visited(graph, current, mut visited)
-	//	neighbour or adjacentEdges
-    //print('\n => Adjacents of ${current} \t are ${adjacent_nodes_not_visited }')
-		
-	for i in adjacent_nodes_not_visited {
-		dfs_recursive(mut visited, graph, i, target )
+	//mut adjacent_nodes_not_visited := [] string {}
+	adjacent_nodes_not_visited := neighbours_not_visited(graph, current, visited)
+	//	neighbours or adjacent nodes NOT VISITED ... with true
+
+	for node in adjacent_nodes_not_visited {
+		dfs_recursive(mut visited, graph, node, target )
 	}
 	
 }	
+// END of recursive DFS
 
-
-
-fn neighbours_not_visited(any_graph map[string] [] string, current string,  mut visited map [string] bool ) [] string {
+fn neighbours_not_visited(a_graph map[string] [] string, current string, visited map [string] bool) [] string {
 	
 	mut not_visited := [] string {} // [type ] string with empty
-	
-	mut all_adj := any_graph[current]  // all adjacents from current because it is map
-	//print('\n => Adjacents of ${current} \t and ${all_adj }')
-	for i in all_adj {
-		if visited[i] == false
+	all_adjacents := a_graph[current]  // all nodes adjacents from current node because it is map
+	//print('\n => Adjacents of ${current} \t and ${all_adjacents}')
+	for node in all_adjacents {
+		if visited[node] == false
 		{
-		 //not_visited << i
-		 not_visited.insert(0, i)
+		 not_visited.insert(0, node)
 		}
 	}
+	//pre-order of dfs ... reverse()
 	return not_visited.reverse()
 }		
 
-
-
-
-// Creating aa map to initialize with of visited nodes .... all with false in the init
+// Creating a map to initialize with of visited nodes .... all with false in the init
 // so these nodes are NOT VISITED YET
 fn visited_init(a_graph map[string][]string ) map[string] bool {
-	mut array_of_keys := a_graph.keys() // get all keys of this map
-	mut temp := map[string] bool {} // attention in these initializations with maps
-	for i in array_of_keys {
-		temp[i] = false
+	array_of_nodes := a_graph.keys() // get all keys of this map ... NODES in this case
+	mut visited_array := map[string] bool {} // attention in these initializations with maps
+	for node in array_of_nodes {
+		visited_array[node] = false
 	}
-	return temp
-}
-
-// Based in the current node that is final, search for his parent, that is already visited, up to the root or start node
-fn build_path_reverse(graph map[string][]string, start string, final string, visited map[string]bool) []string {
-	print('\n\n Nodes visited (true) or no (false): $visited')
-	array_of_nodes := graph.keys()
-	mut current := final
-	mut path := []string{}
-	path << current
-
-	for current != start {
-		for i in array_of_nodes {
-			if (current in graph[i]) && (visited[i] == true) {
-				current = i
-				break // the first ocurrence is enough
-			}
-		}
-		path << current // updating the path tracked
-	}
-	return path
+	return visited_array
 }
 //*****************************************************
